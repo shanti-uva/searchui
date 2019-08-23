c3b57d366acbc1b923797df05fd029c816c5221e
Bill Ferster <bferster@virginia.edu>
2019-08-23 17:02:25 -0400

    header

--- searchui.js
+++ searchui.js
@@ -7,13 +7,15 @@ class SearchUI  {
 		this.top=top;																				// Start of div
 		this.wid=1368;		this.hgt=747-top;
 		this.callback=callback;																		// Callback
-		this.curMode="advanced";																		// Current mode - can be input, simple, or advanced
-		this.curQuery={};																			// Current query
+		this.curMode="input";																		// Current mode - can be input, simple, or advanced
+		this.curQuery={ text:""};																	// Current query
 		this.displayMode="grid";																	// Dispay mode - can be grid, pic, or text
 		this.curType="All";																			// Current item types
 		this.types=["All","Images","Video","Audio","Visuals","Places","Sources","Subjects","Terms"]; // Types
 		this.curPage=0;																				// Current page being shown
 		this.pageSize=100;																			// Results per page	
+		this.pageStart=0;
+		this.numItems=1104;
 		this.AddFrame();																			// Add div framework
 		this.Draw();																				// Draw
 	}
@@ -21,19 +23,44 @@ class SearchUI  {
 	AddFrame()																					// ADD DIV FRAMEWORK
 	{
 		var str="<div id='sui-main' class='sui-main'>";												// Main container
-		str+="<div id='sui-header' class='sui-header'>";											// Header
+		var _this=this;																				// Save context
+		str+="<div id='sui-header' class='sui-header'>";											// Header 
 		str+="<div id='sui-headLeft'  class='sui-headLeft'></div>";									// Left header
-		str+="<div id='sui-headRight' class='sui-headRight'></div></div>";							// Right
+		str+="<div id='sui-headRight' class='sui-headRight'>";										// Right header - open
+		str+="<div class='sui-search1'>&#xe623</div>";												// Magnifier
+		str+="<input type='text' id='sui-search' class='sui-search2' placeholder='Enter Search'>"; 	// Search input
+		str+="<div id='sui-clear' class='sui-search3'>&#xe610</div>";								// Clear button
+		str+="<div id='sui-searchgo' class='sui-search4'>&#xe68a</div>";							// Go button
+		str+="<img id='sui-mode' class='sui-search5' src='img/advicon.png'></div></div>";			// Switch mode button - end right header and header			
 		str+="<div id='sui-left' class='sui-left'>";												// Left side
 		str+="<div id='sui-results' class='sui-results'></div>";									// Results
 		str+="<div id='sui-footer' class='sui-footer'></div>";										// Footer
 		str+="<div id='sui-right' class='sui-right'></div></div>";									// Right side
 		$("body").append(str);																		// Add framework to body
-	}
 
-	Draw()																						// DRAW SEARCH
+		$("#sui-clear").on("mouseover",function() { $(this).html("&#xe60d"); });					// Highlight						
+		$("#sui-clear").on("mouseout", function() { $(this).html("&#xe610"); });					// Normal						
+		$("#sui-clear").on("click", function() { 													// ON ERASE
+			$("#sui-search").val("");	_this.curQuery.text=""; 									// Clear input and query												
+			_this.Draw(); 																			// Redraw
+			});					
+		$("#sui-search").on("change", function() { 													// ON SEARCH CHANGE
+			_this.curQuery.text=$(this).val(); 														// Get query
+			if (_this.curMode == "input") _this.curMode="simple";									// Toggle simple mode
+			_this.Draw(); 																			// Redraw
+			});	
+		$("#sui-searchgo").on("click", function() { 												// ON SEARCH GO
+			_this.curQuery.text=$("#sui-search").val(); 											// Get query
+			if (_this.curMode == "input") _this.curMode="simple";									// Toggle simple mode
+			_this.Draw(); 																			// Redraw
+			});	
+		}
+
+	Draw(mode)																						// DRAW SEARCH
 	{
+		if (mode) this.curMode=mode;																// If mode spec'd, use it
 		$("#sui-main").css({ top: this.top, height:this.hgt+"px", width:this.wid+"px" });			// Position main area
+		this.DrawSearchInput();																		// Draw searchinput
 		this.DrawResults();																			// Draw results page if active
 		this.DrawSearchUI();																		// Draw search UI if active
 	}
@@ -45,7 +72,6 @@ class SearchUI  {
 			$("#sui-left").css({ display:"none" });													// Hide results
 			$("#sui-right").css({ display:"none" });												// Hide search ui
 			$("#sui-headLeft").css({ display:"none" });												// Hide left header
-			trace(123)
 			return;																					// Quit
 			}
 		else if (this.curMode == "simple") {														// Simple search
@@ -56,16 +82,24 @@ class SearchUI  {
 			$("#sui-left").css({ width:this.wid-$("#sui-right").width()+"px", display:"block" });	// Size  and show results area
 			$("#sui-right").css({ display:"inline-block" });										// Show search ui
 			}
-		$("#sui-header").css({"background-color":"#ccc"} );											// Fill header	
+		$("#sui-header").css({"background-color":"#aaa"} );											// Fill header	
 		$("#sui-headLeft").css({ display:"inline-block" });											// Show left header
 		this.DrawHeader();																			// Draw header
 		this.DrawItems();																			// Draw items
 		this.DrawFooter();																			// Draw footer
 	}
 
-	DrawHeader()																				// DRAW RESULTS HEADER
+	DrawSearchInput()
 	{
 	}
+
+	DrawHeader()																				// DRAW RESULTS HEADER
+	{
+		var str="&nbsp;<span id='sui-resclose' class='sui-resclose'>&#xe60f</span>&nbsp;&nbsp;Search results:";
+		str+="<span style='font-size:12px'> (1-100) of 11408</span>";
+		$("#sui-headLeft").html(str);
+		$("#sui-resclose").on("click", ()=> { this.Draw("input"); });							// ON QUIT
+		}
 
 	DrawItems()																					// DRAW RESULT ITEMS
 	{
